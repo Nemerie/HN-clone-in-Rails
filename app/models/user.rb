d987e01b557e30378a607688319a87f3f01715db
User <user@pop-os.localdomain>
2021-07-19 12:51:53 +0300

    Basic model and signup

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+
+	validates :name, presence: true,
+                   uniqueness: true,
+                   length: { maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
