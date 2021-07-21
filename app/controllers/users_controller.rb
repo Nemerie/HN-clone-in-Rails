6dec58b2ad6c79879c1fabe35e6c4a2dbf217418
User <user@pop-os.localdomain>
2021-07-21 11:54:39 +0300

    Working signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  #def new
+  #  @user = User.new
+
+  #  render :login
+  #end
+
+  def login
   end
 
   def create
@@ -11,13 +16,22 @@ class UsersController < ApplicationController
     
     if @user.save
       # If user saves in the db successfully:
+      log_in @user
       flash[:notice] = "Account created successfully!"
       redirect_to root_path
     else
       # If user fails model validation - probably a bad password or duplicate email:
       flash.now.alert = "Couldn't create account."
-      render :new
+      render :login
     end
+  end
+
+  def show
+    if current_user.nil?
+      redirect_to login_path
+    end
+
+    @user = current_user
   end
 
 private
@@ -25,6 +39,6 @@ private
   def user_params
     # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
     # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  # Add a root route if you don't have one...
-	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
-	root to: 'users#new'
-  	
-  	# sign up page with form:
-	get 'users/new' => 'users#new', as: :new_user
-	
-	# create (post) action for when sign up form is submitted:
-	post 'users' => 'users#create'
+	root 'users#show'
+  
+	get    '/me',     to: 'users#show'
+	get    '/login',  to: 'users#login'
+	post   '/signup', to: 'users#create'
+	post   '/login',  to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	get    '/logout', to: 'sessions#destroy' # TODO: remove
 end
