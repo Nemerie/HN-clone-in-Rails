c07c30c9414e4476313fc503b9b319b46afe06e9
User <user@pop-os.localdomain>
2021-08-16 13:58:13 +0300

    Controller tests for posts and comments

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:example_comment)
+    @post = posts(:example_post)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Comment.count' do
+      post comments_new_path, params: { content: "random content",
+                                        post: @post,
+                                        parent: @comment }
+    end
+    assert_redirected_to login_url
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:example_post)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Post.count' do
+      post submit_path, params: { post: { title: "test title",
+                                          url: "https://testurl.com" } }
+    end
+    assert_redirected_to login_url
+  end
 end
